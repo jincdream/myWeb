d4ff7e5f60bd6c27d992d02e761a0066d8a2ecbe
jincdream <275727449@qq.com>
2014-03-08 01:02:56 +0800

    photo 0.2

--- cnij/public/javascripts/photo.js
+++ cnij/public/javascripts/photo.js
@@ -1,4 +1,4 @@
-function Book() {
+function Book() { //css3翻页，可以更完美
 	var count = 0;
 	return function nextP(hBook, sBook) {
 		var hB = hBook.eq(count);
@@ -13,7 +13,7 @@ function Book() {
 				opacity: '1'
 			});
 		} else {
-			sBook.find('img').eq(1).css({
+			sBook.find('img').eq(0).css({
 				transform: 'rotateY(-180deg)'
 			});
 		}
@@ -25,12 +25,16 @@ function Book() {
 	};
 }
 (function() {
-	var stage = $('div.stage');
-	var hBook = stage.find('.hBook');
-	var sBook = stage.find('.sBook');
-	var book = new Book();
+	var stage = $('div.stage'),
+		hBook = stage.find('.hBook'),
+		sBook = stage.find('.sBook'),
+		book = new Book(),
+		inx = 0;
 	$('#next').click(function() {
-		book(hBook, sBook);
+		if (inx <= 3)
+			inx = book(hBook, sBook);
+		else
+			book = null;
 	});
 })();
 
